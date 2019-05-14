33e1b7ce9ec83993e1b69f279c0601841796f7e1
nodejh <jianghangscu@gmail.com>
2019-05-14 11:43:05 +0800

    build mention

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ let plugins = [
   'advlist', 'anchor', 'autolink', 'autoresize', 'autosave', 'bbcode', 'charmap', 'code', 'codesample',
   'colorpicker', 'contextmenu', 'directionality', 'emoticons', 'help', 'fullpage',
   'fullscreen', 'hr', 'image', 'imagetools', 'importcss', 'insertdatetime', 'legacyoutput', 'link',
-  'lists', 'media', 'nonbreaking', 'noneditable', 'pagebreak', 'paste', 'preview', 'print', 'save',
+  'lists', 'media', 'mention', 'nonbreaking', 'noneditable', 'pagebreak', 'paste', 'preview', 'print', 'save',
   'searchreplace', 'spellchecker', 'tabfocus', 'table', 'template', 'textcolor', 'textpattern', 'toc',
   'visualblocks', 'visualchars', 'wordcount', 'quickbars',
 ];
